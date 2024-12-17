b4818944ea7690d40297ab2d20c3e7de2dbc5a4d
LucasTNM <148718831+LucasTNM@users.noreply.github.com>
2024-12-17 14:05:17 -0300

    correção de rotas

--- Backend_CarrosMacqueen/routes/carRoutes.js
+++ Backend_CarrosMacqueen/routes/carRoutes.js
@@ -1,20 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const carController = require('../controller/carController');
-const clientController = require('../controller/clientController'); // Importa o controlador de clientes
 
 // Rotas CRUD para carros
-router.post('/cars', carController.createCar);
-router.get('/cars', carController.getCars);
-router.get('/cars/:name', carController.getCarByName);
-router.put('/cars/:name', carController.updateCarByName);
-router.delete('/cars/:name', carController.deleteCarByName);
-router.get('/cars/class/:class', carController.getCarsByClass);
-router.get('/cars/year/:year', carController.getCarsByYear);
-router.get('/cars/price/:min/:max', carController.getCarsByPriceRange);
-router.put('/cars/:name/image', carController.updateCarImageByName);
-router.get('/cars/recent', carController.getRecentCars);
-router.get('/cars/expensive', carController.getExpensiveCars);
-router.get('/cars/cheap', carController.getCheapCars);
+router.post('/', carController.createCar);
+router.get('/', carController.getCars);
+router.get('/:name', carController.getCarByName);
+router.put('/:name', carController.updateCarByName);
+router.delete('/:name', carController.deleteCarByName);
+router.get('/class/:class', carController.getCarsByClass);
+router.get('/year/:year', carController.getCarsByYear);
+router.get('/price/:min/:max', carController.getCarsByPriceRange);
+router.put('/:name/image', carController.updateCarImageByName);
+router.get('/recent', carController.getRecentCars);
+router.get('/expensive', carController.getExpensiveCars);
+router.get('/cheap', carController.getCheapCars);
 
 module.exports = router;
--- Backend_CarrosMacqueen/routes/clientRoutes.js
+++ Backend_CarrosMacqueen/routes/clientRoutes.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const clientController = require('../controller/clientController'); // Importa o controlador de clientes
+const clientController = require('../controller/clientController');
 
 // Rotas CRUD para clientes
-router.post('/clients', clientController.createClient);
-router.get('/clients', clientController.getClients);
-router.get('/cpf/:cpf', clientController.getClientByCPF); // Atualiza a rota para buscar pelo CPF
-router.put('/:id', clientController.updateClientById);
-router.delete('/:id', clientController.deleteClientById);
+router.post('/', clientController.createClient);
+router.get('/', clientController.getClients);
+
+// Outras rotas para clientes podem ser adicionadas aqui
 
 module.exports = router;
