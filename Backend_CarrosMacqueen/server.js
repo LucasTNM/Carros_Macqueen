87316a81e805976d2a7b6ce737d7b9ea4a8e060b
LucasTNM <148718831+LucasTNM@users.noreply.github.com>
2025-02-21 12:11:42 -0300

    Fix integração

--- Backend_CarrosMacqueen/server.js
+++ Backend_CarrosMacqueen/server.js
@@ -31,13 +31,6 @@ app.use(
 
 app.use(express.json());
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-  next();
-});
-
 app.use('/api/cars', require('./routes/carRoutes.js'));
 app.use('/api/clients', require('./routes/clientRoutes.js'));
 app.use('/api/cart', require('./routes/cartRoutes.js'));
