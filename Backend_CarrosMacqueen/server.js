ab406987a46d4db0878b3b78c894949a3f76b86c
LucasTNM <148718831+LucasTNM@users.noreply.github.com>
2025-02-21 00:33:28 -0300

    Cors Update

--- Backend_CarrosMacqueen/server.js
+++ Backend_CarrosMacqueen/server.js
@@ -8,9 +8,21 @@ connectDB();
 
 const app = express();
 
+const allowedOrigins = [
+  'https://carros-macqueen-frontend.vercel.app',
+  'http://localhost:3000' // Adicione outros domínios permitidos aqui
+];
+
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "https://carros-macqueen-frontend.vercel.app/", // Permite apenas este URL
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
     methods: ["GET", "POST", "PUT", "DELETE"], // Métodos HTTP permitidos
     credentials: true, // Permite cookies ou headers personalizados
   })
